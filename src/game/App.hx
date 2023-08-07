158c3e5c0de23ce995759bcdf4dbb17d7e91d13d
Brian Ganster <BGanster96@gmail.com>
2023-08-06 21:06:51 -0700

    disabled full screen

--- src/game/App.hx
+++ src/game/App.hx
@@ -175,7 +175,7 @@ class App extends dn.Process {
 		// Engine settings
 		engine.backgroundColor = 0xff<<24 | 0x111133;
         #if( hl && !debug )
-        engine.fullScreen = true;
+        // engine.fullScreen = true;
         #end
 
 		#if( hl && !debug)
